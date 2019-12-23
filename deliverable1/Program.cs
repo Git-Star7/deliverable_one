8663e19ea895c7fff341d29d0fb3dee33598c91b
Michael <Michael@acdmail.com>
2019-12-23 10:58:41 -0500

    base code

--- deliverable1/Program.cs
+++ deliverable1/Program.cs
@@ -6,7 +6,44 @@ namespace deliverable1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello New World!");
+            Console.WriteLine("What kind of trip would you like to go on, musical, tropical, or adventurous?");
+            string vacationType = Console.ReadLine();
+            string destination;
+            if (vacationType == "musical")
+            {
+                destination = "New Orleans";
+            }
+            else if (vacationType == "tropical")
+            {
+                destination = " a beach vacation in Mexico";
+            }
+            else
+            {
+                destination = "a whitewater rafting trip at the Grand Canyon";
+            }
+
+
+            Console.WriteLine("How many are in your group?");
+            int groupSize = int.Parse(Console.ReadLine());
+            string travelSuggestion;
+            
+            if (groupSize >= 1 && groupSize <= 2)
+            {
+                travelSuggestion = "first class flight";
+            }
+            else if (groupSize >= 3 && groupSize <= 5)
+            {
+                travelSuggestion = "helicopter ride";
+            }
+            else
+            {
+                travelSuggestion = "charter flight";
+            }
+
+
+            string result = $"Since you’re a group of {groupSize} going on a {vacationType} trip, you should take a {travelSuggestion} to {destination}";
+
+            Console.WriteLine(result);
         }
     }
 }
